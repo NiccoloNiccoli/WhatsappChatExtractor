e2d8bed6760d28a911c14c1554ef0af70863e269
NiccoloNiccoli <niccolo.niccoli@stud.unifi.it>
2021-01-21 23:41:45 +0100

    Alpha version

--- Extractor.cpp
+++ Extractor.cpp
@@ -35,30 +35,37 @@ int Extractor::extract(File file) {
                     date = tmpString;
                     //now it's time for the sender
                     tmpString = "";
-                    for (int q = 3; j + q != currentString.end(); ++j) {
+                    for (int q = 3; j + q != currentString.end()-1; ++j) {
                         tmpString += *(j + q) + " ";
                     }
-                    k = tmpString.find(":");
+                    /*k = tmpString.find(":");
                     for (int q = 0; q < k; ++q)
                         sender += tmpString.at(q);
+                    */
+                    tmpString.erase(tmpString.end()-2,tmpString.end());
+                    sender = tmpString;
                     break;
                 }
-                else if((*j == "<span" && *(j+1) == "dir=\"ltr\"")){//from this/the next one/two lines after the text begins
+                else if((*(j) == "dir=\"ltr\"")){//from this/the next one/two lines after the text begins
                     //now I check every word until I find <span>, which is the thing before the message
                     std::string tmpString =*i;
-                    tmpString.erase(tmpString.begin(), tmpString.begin()+tmpString.find('<')+1);
+                    /*tmpString.erase(tmpString.begin(), tmpString.begin()+tmpString.find('<')+1);
                     auto beginning = tmpString.find_first_of('<');
                     beginning+=5; //for span>
                     tmpString.erase(tmpString.begin(),tmpString.begin()+beginning);
                     auto end = tmpString.find('<');
-                    tmpString.erase(tmpString.end()-tmpString.size()+end,tmpString.end());
+                    tmpString.erase(tmpString.end()-tmpString.size()+end,tmpString.end());*/
+                    tmpString.erase(tmpString.begin(), tmpString.begin()+tmpString.find("<span>")+6);
+                    auto it = std::find(tmpString.begin(),tmpString.end(),'<');
+                    if(it != tmpString.end())
+                        tmpString.erase(tmpString.begin()+tmpString.find('<'),tmpString.end());
                     msg = tmpString;
                     break;
                 }
             }
             ++i;
         }
-        std::cout<<date<< ", " <<time<<" - "<<sender<<":"<<msg<<std::endl;
+        std::cout<<date<< ", " <<time<<" - "<<sender<<": "<<msg<<std::endl;
 
     }
 }
--- main.cpp
+++ main.cpp
@@ -3,12 +3,18 @@
 #include "Extractor.h"
 
 int main() {
-    std::string fileName = "C:\\Users\\super\\OneDrive\\Desktop\\chat2.txt";
+    std::string fileName = "C:\\Users\\super\\OneDrive\\Desktop\\data.txt";
     /*std::cout<<"Insert the full path of your file (Ex: C:\\Users\\Me\\Desktop\\chat.txt): "<<std::endl;
     std::cin>>fileName;
     std::cout<<std::endl;*/
     File file(fileName);
     Extractor extractor;
     extractor.extract(file);
+    /*TODO
+     * stampare la chat in un file di testo
+     * leggere anche i messaggi _ * ~ ```
+     * leggere i messaggi delle foto
+     * leggere i messaggi multi-riga
+     * */
     return 0;
 }
